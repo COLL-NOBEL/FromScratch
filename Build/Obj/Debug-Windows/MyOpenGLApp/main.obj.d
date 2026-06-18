d694f094b93efd4a4544223917521fa4c598fed9
COLL-NOBEL <collinsalloh@gmail.com>
2026-06-18 17:10:53 +0100

    Still part of the initial commit

--- Build/Obj/Debug-Windows/MyOpenGLApp/main.obj.d
+++ Build/Obj/Debug-Windows/MyOpenGLApp/main.obj.d
@@ -1,26 +1,22 @@
 C:\Users\colli\Desktop\FromSratch\Build\Obj\Debug-Windows\MyOpenGLApp\main.obj: \
   C:\Users\colli\Desktop\FromSratch\OpenPrj\src\main.cpp \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\NkWindow.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkMain.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKPlatform\NkPlatformDetect.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkTypes.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NKMath.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkFunctions.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkEntry.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkTraits.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkTypes.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKPlatform\NkArchDetect.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkInline.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKPlatform\NkCompilerDetect.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkAngle.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkLegacySystem.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkExport.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkFormat.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\NkCompat.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkStringUtils.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkString.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\NkCompat.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkStringView.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkExport.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKPlatform\NkCompilerDetect.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkInline.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkFunction.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkAllocator.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkExport.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkUtils.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkTraits.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\Assert\NkAssert.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\Assert\NkAssertion.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\Assert\NkAssertHandler.h \
@@ -31,6 +27,31 @@ C:\Users\colli\Desktop\FromSratch\Build\Obj\Debug-Windows\MyOpenGLApp\main.obj: 
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkVersion.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkMacros.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkCGXDetect.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkStringFormat.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Sequential\NkVector.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\NkContainersExport.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Iterators\NkIterator.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Iterators\NkInitializerList.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Sequential\NkVectorError.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkMemory.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkContainerAllocator.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkAtomic.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkGc.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkHash.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkIntrusivePtr.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkProfiler.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkSharedPtr.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkTracker.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkTag.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKPlatform\NkFoundationLog.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkUniquePtr.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSystem.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkTypes.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NKMath.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkFunctions.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkAngle.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkLegacySystem.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkFormat.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkEulerAngle.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkVec.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkMat.h \
@@ -42,25 +63,11 @@ C:\Users\colli\Desktop\FromSratch\Build\Obj\Debug-Windows\MyOpenGLApp\main.obj: 
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkRectangle.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMath\NkSegment.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkRendererApi.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkStringUtils.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkStringFormat.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkWindowConfig.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSafeArea.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSurfaceHint.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\CacheFriendly\NkArray.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Iterators\NkIterator.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\NkContainersExport.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSurface.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkContext.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSystem.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkWindowId.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkEventSystem.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkEvent.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\NkContainers.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Heterogeneous\NkPair.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Iterators\NkInitializerList.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Sequential\NkVectorError.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Sequential\NkVector.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Associative\NkMap.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\Associative\NkBTree.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\String\NkFormatf.h \
@@ -87,9 +94,6 @@ C:\Users\colli\Desktop\FromSratch\Build\Obj\Debug-Windows\MyOpenGLApp\main.obj: 
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKLogger\NkSink.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKLogger\NkLogMessage.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKLogger\NkFormatter.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkUniquePtr.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkSharedPtr.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKCore\NkAtomic.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKLogger\NkTextFormat.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKLogger\NkSync.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKThreading\NkMutex.h \
@@ -102,28 +106,49 @@ C:\Users\colli\Desktop\FromSratch\Build\Obj\Debug-Windows\MyOpenGLApp\main.obj: 
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKTime\NkDuration.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkGamepadSystem.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkGamepadMappingPersistence.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContainers\CacheFriendly\NkArray.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSurface.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSurfaceHint.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\EntryPoints\NkWindowsDesktop.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\Application.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\Scene.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\GameObject.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\Transform.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\Math.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkVec2.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkVec3.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkVec4.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkMat3x3.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkMat4x4.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkQuat.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkMathUtils.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\nkMath\NkColor.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Renderer.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Camera.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\..\core\Math.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Light.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Material.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Shader.hpp \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKGlad\include\glad\gl.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\NKGlad\include\KHR\khrplatform.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Texture.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\Mesh.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\VertexBuffer.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\graphics\..\core\Transform.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\Time.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\Input.hpp \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkKeycodeMap.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\core\..\core\Math.hpp \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\NkWindow.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkWindowConfig.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkSafeArea.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkContext.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkEvent.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkDropSystem.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKEvent\NkEventDispatcher.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkWindow.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Platform\Win32\NkWin32Window.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkDialogs.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkEntry.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkMemory.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkContainerAllocator.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkGc.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkHash.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkIntrusivePtr.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkProfiler.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkTracker.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKMemory\NkTag.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKPlatform\NkFoundationLog.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\Core\NkMain.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKWindow\EntryPoints\NkWindowsDesktop.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKGlad\include\glad\wgl.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\NKGlad\include\glad\gl.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\NKGlad\include\KHR\khrplatform.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKGlad\include\glad\gl.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Factory\NkContextFactory.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Core\NkIGraphicsContext.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Core\NkContextDesc.h \
@@ -136,4 +161,8 @@ C:\Users\colli\Desktop\FromSratch\Build\Obj\Debug-Windows\MyOpenGLApp\main.obj: 
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Graphics\DirectX\NkDirectXContextData.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Graphics\Software\NkSoftwareContext.h \
   C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Core\NkSurfaceDesc.h \
-  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Graphics\Software\NkSWPixel.h
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKContext\Graphics\Software\NkSWPixel.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\nkentseu\include\NKGlad\include\glad\wgl.h \
+  C:\Users\colli\Desktop\FromSratch\Externals\libs\NKGlad\include\glad\gl.h \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\DemoScene.hpp \
+  C:\Users\colli\Desktop\FromSratch\OpenPrj\src\..\engine\graphics\ShaderLoader.hpp
